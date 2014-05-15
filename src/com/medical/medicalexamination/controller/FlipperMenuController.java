62ca046897d0043377b00f0ff16932a02f3e7ce1
jugo <jugo.tw@gmail.com>
2014-05-16 02:12:55 +0800

    add

---
+++ src/com/medical/medicalexamination/controller/FlipperMenuController.java
@@ -0,0 +1,162 @@
+package com.medical.medicalexamination.controller;
+
+import com.medical.medicalexamination.R;
+import com.medical.medicalexamination.model.EventHandler;
+import com.medical.medicalexamination.model.EventMessage;
+import com.medical.medicalexamination.model.Logs;
+import com.medical.medicalexamination.model.Type;
+import com.medical.medicalexamination.view.FlipperView;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class FlipperMenuController
+{
+
+	private FlipperView		flipperView			= null;
+	private MenuID			menuId				= null;
+	//	private RelativeLayout	loginMainLayout		= null;
+	private RelativeLayout	historyMainLayout	= null;
+	private RelativeLayout	calendarLayout		= null;
+	private Handler			notifyHandler		= null;
+	private LoginController	loginController		= null;
+
+	public class LoginData
+	{
+		public String	mstrName		= null;
+		public String	mstrPassword	= null;
+	}
+
+	private class MenuID
+	{
+		public int	mnLoginId			= Type.INVALID;
+		public int	mnHistoryId			= Type.INVALID;
+		public int	mnCalendarId		= Type.INVALID;
+		public int	mnSubscribeId		= Type.INVALID;
+		public int	mnAccountAdd		= Type.INVALID;
+		public int	mnForgetPassword	= Type.INVALID;
+	}
+
+	public FlipperMenuController(Activity activity)
+	{
+		super();
+		init(activity);
+	}
+
+	private void init(Activity activity)
+	{
+		menuId = new MenuID();
+
+		flipperView = (FlipperView) activity.findViewById(R.id.fliper_menu_option);
+		if (null == flipperView)
+		{
+			Logs.showTrace("Flipper view is invalid");
+			return;
+		}
+
+		menuId.mnLoginId = flipperView.addChild(R.layout.login);
+		menuId.mnHistoryId = flipperView.addChild(R.layout.history);
+		menuId.mnCalendarId = flipperView.addChild(R.layout.calendar);
+
+		loginController = new LoginController(activity);
+		//		loginMainLayout = (RelativeLayout) flipperView.findViewById(R.id.login_main_layout);
+		//		loginMainLayout.setOnTouchListener(mainLayoutTouch);
+		//		loginHandle(activity);
+
+		historyMainLayout = (RelativeLayout) flipperView.findViewById(R.id.history_main_layout);
+		historyMainLayout.setOnTouchListener(mainLayoutTouch);
+		historyHandler(activity);
+
+		calendarLayout = (RelativeLayout) flipperView.findViewById(R.id.calendar_main_layout);
+		calendarLayout.setOnTouchListener(mainLayoutTouch);
+
+	}
+
+	public void setNotifyHandler(Handler handler)
+	{
+		flipperView.setNotifyHandler(handler);
+		notifyHandler = handler;
+		loginController.setNotifyHandler(handler);
+	}
+
+	public void setHideEnable(boolean bEnable)
+	{
+		flipperView.setHideEnable(bEnable);
+	}
+
+	public void showLogin()
+	{
+		flipperView.showView(menuId.mnLoginId);
+	}
+
+	public void showHistory()
+	{
+		flipperView.showView(menuId.mnHistoryId);
+	}
+
+	public void showCalendar()
+	{
+		flipperView.showView(menuId.mnCalendarId);
+	}
+
+	public void close()
+	{
+		flipperView.close();
+	}
+
+	//	private void loginHandle(final Activity activity)
+	//	{
+	//
+	//		TextView loginBtn = (TextView) loginMainLayout.findViewById(R.id.start_login);
+	//		loginBtn.setOnClickListener(new OnClickListener()
+	//		{
+	//			@Override
+	//			public void onClick(View v)
+	//			{
+	//				EditText editText = (EditText) loginMainLayout.findViewById(R.id.login_name);
+	//				String strName = editText.getText().toString();
+	//				editText = (EditText) loginMainLayout.findViewById(R.id.login_password);
+	//				String strPassword = editText.getText().toString();
+	//
+	//				if (null != strName && 0 < strName.length() && null != strPassword && 0 < strPassword.length())
+	//				{
+	//					LoginData data = new LoginData();
+	//					data.mstrName = strName;
+	//					data.mstrPassword = strPassword;
+	//					EventHandler.notify(notifyHandler, EventMessage.MSG_LOGIN, 0, 0, data);
+	//					data = null;
+	//					flipperView.close();
+	//				}
+	//
+	//			}
+	//		});
+	//
+	//	}
+
+	private void historyHandler(final Activity activity)
+	{
+
+	}
+
+	private OnTouchListener	mainLayoutTouch	= new OnTouchListener()
+											{
+
+												@Override
+												public boolean onTouch(View v, MotionEvent event)
+												{
+													switch (event.getAction())
+													{
+													case MotionEvent.ACTION_DOWN:
+														return true;
+													}
+													return false;
+												}
+											};
+}
---
+++ src/com/medical/medicalexamination/controller/LoginController.java
@@ -0,0 +1,93 @@
+package com.medical.medicalexamination.controller;
+
+import com.medical.medicalexamination.R;
+import com.medical.medicalexamination.controller.FlipperMenuController.LoginData;
+import com.medical.medicalexamination.model.EventHandler;
+import com.medical.medicalexamination.model.EventMessage;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class LoginController
+{
+	private Activity		theActivity		= null;
+	private RelativeLayout	loginMainLayout	= null;
+	private Handler			notifyHandler	= null;
+
+	public class LoginData
+	{
+		public String	mstrName		= null;
+		public String	mstrPassword	= null;
+	}
+
+	public LoginController(Activity activity)
+	{
+		super();
+		theActivity = activity;
+		loginMainLayout = (RelativeLayout) activity.findViewById(R.id.login_main_layout);
+		loginMainLayout.setOnTouchListener(mainLayoutTouch);
+		loginHandle(activity);
+	}
+
+	@Override
+	protected void finalize() throws Throwable
+	{
+		super.finalize();
+	}
+
+	public void setNotifyHandler(Handler handler)
+	{
+		notifyHandler = handler;
+	}
+
+	private void loginHandle(final Activity activity)
+	{
+		TextView loginBtn = (TextView) loginMainLayout.findViewById(R.id.start_login);
+		loginBtn.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				EditText editText = (EditText) loginMainLayout.findViewById(R.id.login_name);
+				String strName = editText.getText().toString();
+				editText = (EditText) loginMainLayout.findViewById(R.id.login_password);
+				String strPassword = editText.getText().toString();
+
+				if (null != strName && 0 < strName.length() && null != strPassword && 0 < strPassword.length())
+				{
+					LoginData data = new LoginData();
+					data.mstrName = strName;
+					data.mstrPassword = strPassword;
+					EventHandler.notify(notifyHandler, EventMessage.MSG_LOGIN, 0, 0, data);
+					data = null;
+					//		flipperView.close();
+				}
+
+			}
+		});
+
+	}
+
+	private OnTouchListener	mainLayoutTouch	= new OnTouchListener()
+											{
+
+												@Override
+												public boolean onTouch(View v, MotionEvent event)
+												{
+													switch (event.getAction())
+													{
+													case MotionEvent.ACTION_DOWN:
+														return true;
+													}
+													return false;
+												}
+											};
+
+}
