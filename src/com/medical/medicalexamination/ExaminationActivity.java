9adfb6d6b907fb86d15c57569fdc37d5a4b9de12
jugo <jugo.tw@gmail.com>
2014-06-11 13:20:40 +0800

    add

---
+++ src/com/medical/medicalexamination/ExaminationActivity.java
@@ -0,0 +1,15 @@
+package com.medical.medicalexamination;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExaminationActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+
+}
---
+++ src/com/medical/medicalexamination/PersonInfoActivity.java
@@ -0,0 +1,16 @@
+package com.medical.medicalexamination;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PersonInfoActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.person_info);
+	}
+
+}
