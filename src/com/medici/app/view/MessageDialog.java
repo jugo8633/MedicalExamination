a0810c4fba8d83bf1bb5a10620adc2ab89e468ae
jugo <jugo.tw@gmail.com>
2014-06-16 12:13:17 +0800

    add

---
+++ src/com/medici/app/view/MessageDialog.java
@@ -0,0 +1,15 @@
+package com.medici.app.view;
+
+import android.app.Activity;
+
+public class MessageDialog
+{
+	private Activity	theActivity	= null;
+
+	public MessageDialog(Activity activity)
+	{
+		super();
+	}
+
+	
+}
