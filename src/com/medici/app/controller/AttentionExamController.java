c811e133d10529b8403ee9be697fa563e3dd1dc5
jugo <jugo.tw@gmail.com>
2014-06-30 17:13:57 +0800

    update

--- src/com/medici/app/controller/AttentionExamController.java
+++ src/com/medici/app/controller/AttentionExamController.java
@@ -1,11 +1,17 @@
 package com.medici.app.controller;
 
+import com.jugo.smartwidget.common.Logs;
+import com.jugo.smartwidget.common.Utility;
+import com.jugo.smartwidget.image.BitmapHandler;
 import com.medici.app.R;
 import com.medici.app.model.EventMessage;
 import com.medici.app.model.Global;
 import com.medici.app.model.Type;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 import android.os.Handler;
 import android.os.Message;
 import android.view.ViewGroup;
@@ -15,9 +21,12 @@ import android.widget.TextView;
 
 public class AttentionExamController extends TestAreaController
 {
-
-	private int		mnExamResult		= 0;
-	private int[]	listImgViewResId	= { R.id.imageViewCheck };
+	private int			mnLevel				= 0;
+	private int			mnExamResult		= 0;
+	private int[]		listImgViewResId	= { R.id.imageViewCheck };
+	private Bitmap		bmpDay				= null;
+	private Bitmap		bmpDayMosaic		= null;
+	private ImageView	ivDay				= null;
 
 	public AttentionExamController(Activity activity, Handler handler)
 	{
@@ -30,6 +39,7 @@ public class AttentionExamController extends TestAreaController
 		ViewGroup parent = (ViewGroup) activity.findViewById(R.id.RelativeLayoutAttentionMain);
 		initHeader(parent);
 		addImageViewResId(parent, listImgViewResId, selfHandler);
+		ivDay = (ImageView) parent.findViewById(R.id.imageViewDay);
 	}
 
 	private void showInfo()
@@ -43,16 +53,35 @@ public class AttentionExamController extends TestAreaController
 		setExaminationMode(bSet);
 	}
 
+	private void setMosaic(int nLevel)
+	{
+		releaseAllBitmap();
+		bmpDay = BitmapFactory.decodeResource(theActivity.getResources(), R.drawable.day13);
+		Rect rect = Utility.getViewRect(ivDay);
+		//	bmpDayMosaic = BitmapHandler.makeMosaic(bmpDay, rect, 40);
+		bmpDayMosaic = BitmapHandler.scaleBitmap(bmpDay, 10);
+		ivDay.setImageBitmap(bmpDayMosaic);
+		Logs.showTrace("mosaic #################################");
+	}
+
+	private void releaseAllBitmap()
+	{
+		BitmapHandler.releaseBitmap(bmpDay);
+		BitmapHandler.releaseBitmap(bmpDayMosaic);
+	}
+
 	@Override
 	public void init()
 	{
 		release();
 		showInfo();
+		setMosaic(mnLevel);
 	}
 
 	@Override
 	public void release()
 	{
+		mnLevel = 0;
 		mnExamResult = 0;
 	}
 
